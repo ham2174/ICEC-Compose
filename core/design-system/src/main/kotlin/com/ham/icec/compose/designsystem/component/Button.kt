9440b95b8f826e2d54f75a3635cf28588406cd7a
GunHyung Ham <gdreagon@naver.com>
2024-05-29 18:53:45 +0900

    [feat] : IcecTopBarTrailingButton 제작

--- core/design-system/src/main/kotlin/com/ham/icec/compose/designsystem/component/Button.kt
+++
@@ -1,2 +0,0 @@
-package com.ham.icec.compose.designsystem.component
-
---
+++ core/design-system/src/main/kotlin/com/ham/icec/compose/designsystem/component/IcecTopBarTrailingButton.kt
@@ -0,0 +1,44 @@
+package com.ham.icec.compose.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.ham.icec.compose.designsystem.modifier.clickableSingle
+import com.ham.icec.compose.designsystem.modifier.clickableSingleNoRipple
+import com.ham.icec.compose.designsystem.theme.IcecTheme
+
+@Composable
+fun IcecTopBarTrailingButton(
+    modifier: Modifier,
+    background: Color,
+    text: String,
+    textColor: Color,
+    onclick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = background,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .clickableSingleNoRipple { onclick() }
+            .clip(RoundedCornerShape(20.dp))
+            .padding(horizontal = 14.5f.dp, vertical = 6.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = IcecTheme.typography.sbt2,
+            color = textColor
+        )
+    }
+}
