44ccafba56e34e551d268d40b3147c23f012ea89
GunHyung Ham <gdreagon@naver.com>
2024-06-24 17:14:13 +0900

    [fix] : 모델 종료시 재사용 불가능 하던 오류 수정

--- core/face-detection/src/main/kotlin/com/ham/icec/compose/facedetection/di/FaceDetectorOptionsModule.kt
+++ core/face-detection/src/main/kotlin/com/ham/icec/compose/facedetection/di/FaceDetectorOptionsModule.kt
@@ -1,7 +1,5 @@
 package com.ham.icec.compose.facedetection.di
 
-import com.google.mlkit.vision.face.FaceDetection
-import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import dagger.Module
 import dagger.Provides
@@ -12,28 +10,22 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class FaceDetectorModule {
+class FaceDetectorOptionsModule {
 
     @Provides
     @Singleton
-    @Named("FastDetector")
-    fun provideFastDetectorClient(): FaceDetector {
-        val options = FaceDetectorOptions.Builder()
+    @Named("FastMode")
+    fun provideFastDetectorOptions(): FaceDetectorOptions =
+        FaceDetectorOptions.Builder()
             .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
             .build()
 
-        return FaceDetection.getClient(options)
-    }
-
     @Provides
     @Singleton
-    @Named("AccurateDetector")
-    fun provideAccurateDetectorClient(): FaceDetector {
-        val options = FaceDetectorOptions.Builder()
+    @Named("AccurateMode")
+    fun provideAccurateDetectorOptions(): FaceDetectorOptions =
+        FaceDetectorOptions.Builder()
             .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
             .build()
-
-        return FaceDetection.getClient(options)
-    }
 
 }
--- core/face-detection/src/main/kotlin/com/ham/icec/compose/facedetection/service/DetectionServiceImpl.kt
+++ core/face-detection/src/main/kotlin/com/ham/icec/compose/facedetection/service/DetectionServiceImpl.kt
@@ -2,7 +2,9 @@ package com.ham.icec.compose.facedetection.service
 
 import android.graphics.Rect
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import com.ham.icec.compose.utilandroid.extension.toBitmap
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -15,39 +17,45 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
 class DetectionServiceImpl @Inject constructor(
-    @Named("FastDetector") private val fastDetector: FaceDetector,
-    @Named("AccurateDetector") private val accurateDetector: FaceDetector,
+    @Named("FastMode") private val fastDetectorOptions: FaceDetectorOptions,
+    @Named("AccurateMode") private val accurateDetectorOptions: FaceDetectorOptions,
 ) : DetectionService {
 
     override fun getFastDetectFaces(image: ByteArray): Flow<List<Rect>> = flow {
-        emit(fastDetector.process(image))
+        val detector = FaceDetection.getClient(fastDetectorOptions)
+        val result = detector.process(image)
+
+        emit(result)
     }.flowOn(Dispatchers.Default)
 
     override fun getAccurateDetectFaces(image: ByteArray): Flow<List<Rect>> = flow {
-        emit(accurateDetector.process(image))
+        val detector = FaceDetection.getClient(accurateDetectorOptions)
+        val result = detector.process(image)
+
+        emit(result)
     }.flowOn(Dispatchers.Default)
 
-    private suspend fun FaceDetector.process(image: ByteArray): List<Rect> =
-        suspendCancellableCoroutine { continuation ->
-            val inputImage = InputImage.fromBitmap(image.toBitmap(), 0)
+}
 
-            this.process(inputImage)
-                .addOnSuccessListener { faces ->
-                    continuation.resume(faces.map { it.boundingBox })
-                }
-                .addOnFailureListener { e ->
-                    continuation.resumeWithException(e)
-                }
-                .addOnCanceledListener {
-                    continuation.cancel()
-                }
-                .addOnCompleteListener { _ ->
-                    this.close()
-                }
+private suspend fun FaceDetector.process(image: ByteArray): List<Rect> =
+    suspendCancellableCoroutine { continuation ->
+        val inputImage = InputImage.fromBitmap(image.toBitmap(), 0)
+        val task = this.process(inputImage)
 
-            continuation.invokeOnCancellation {
+        task.addOnSuccessListener { faces ->
+            continuation.resume(faces.map { it.boundingBox })
+        }
+            .addOnFailureListener { e ->
+                continuation.resumeWithException(e)
+            }
+            .addOnCanceledListener {
+                continuation.cancel()
+            }
+            .addOnCompleteListener { _ ->
                 this.close()
             }
-        }
 
-}
+        continuation.invokeOnCancellation {
+            this.close()
+        }
+    }
