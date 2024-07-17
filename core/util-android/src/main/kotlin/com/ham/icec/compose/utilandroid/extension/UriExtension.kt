dd56bf767e65a070a6541296ba010f535d3618bf
GunHyung Ham <gdreagon@naver.com>
2024-07-18 03:01:18 +0900

    [feat] : UriExtenstion - Uri.toByteArray() 확장함수 제작

--- core/util-android/src/main/kotlin/com/ham/icec/compose/utilandroid/extension/UriExtension.kt
+++ core/util-android/src/main/kotlin/com/ham/icec/compose/utilandroid/extension/UriExtension.kt
@@ -19,8 +19,21 @@ fun Uri.resizedByteArray(
         outputStream.toByteArray()
     }
 }
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.InputStream
 
 fun Uri.toBitmap(context: Context): Bitmap =
     context.contentResolver.openInputStream(this)?.use { inputStream ->
         BitmapFactory.decodeStream(inputStream)
     } ?: throw IllegalArgumentException("uri로부터 bitmap을 생성할 수 없습니다.") // TODO : Custom Exception 정의 - Uri 이미지 Bitmap으로 변환 실패
+
+suspend fun Uri.toByteArray(context: Context): ByteArray? = withContext(Dispatchers.IO) {
+    try {
+        val inputStream: InputStream? = context.contentResolver.openInputStream(this@toByteArray)
+        inputStream?.readBytes()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
