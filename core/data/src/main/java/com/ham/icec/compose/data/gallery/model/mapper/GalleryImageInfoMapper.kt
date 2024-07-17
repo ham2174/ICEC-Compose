ca92f2294c21c1aab045424495dafda6139d8a83
GunHyung Ham <gdreagon@naver.com>
2024-07-18 02:57:01 +0900

    [refactor] : gallery 도메인 모델 변경에 따른 data 레이어 변경

--- core/data/src/main/java/com/ham/icec/compose/data/gallery/model/mapper/GalleryImageInfoMapper.kt
+++ core/data/src/main/java/com/ham/icec/compose/data/gallery/model/mapper/GalleryImageInfoMapper.kt
@@ -1,14 +1,14 @@
 package com.ham.icec.compose.data.gallery.model.mapper
 
-import com.ham.icec.compose.domain.gallery.entity.Image
 import com.ham.icec.compose.data.gallery.model.GalleryImageInfo
+import com.ham.icec.compose.domain.gallery.entity.MediaStoreImage
 
-fun GalleryImageInfo.toDomain(): Image =
-    Image(
+fun GalleryImageInfo.toDomain(): MediaStoreImage =
+    MediaStoreImage(
         id = id,
         path = uri.toString(),
         orientation = orientation
     )
 
-fun List<GalleryImageInfo>.toDomain(): List<Image> =
+fun List<GalleryImageInfo>.toDomain(): List<MediaStoreImage> =
     map { it.toDomain() }
--- core/data/src/main/java/com/ham/icec/compose/data/gallery/repository/GalleryRepositoryImpl.kt
+++ core/data/src/main/java/com/ham/icec/compose/data/gallery/repository/GalleryRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.ham.icec.compose.data.gallery.repository
 
 import com.ham.icec.compose.data.datasource.local.GalleryDataSource
 import com.ham.icec.compose.data.gallery.model.mapper.toDomain
-import com.ham.icec.compose.domain.gallery.entity.Image
+import com.ham.icec.compose.domain.gallery.entity.MediaStoreImage
 import com.ham.icec.compose.domain.gallery.repository.GalleryRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -12,7 +12,7 @@ class GalleryRepositoryImpl @Inject constructor(
     private val localImageDataSource: GalleryDataSource
 ) : GalleryRepository {
 
-    override fun loadLocalImages(page: Int): Flow<List<Image>> =
+    override fun loadLocalImages(page: Int): Flow<List<MediaStoreImage>> =
         localImageDataSource.fetchMediaStoreImages(page = page).map { it.toDomain() }
 
 }
