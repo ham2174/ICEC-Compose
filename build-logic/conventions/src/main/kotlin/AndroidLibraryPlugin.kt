4af6a9c33e219183e97e68a9a12c75d30563b69f
함건형 <ham121985@gmail.com>
2025-09-17 20:55:26 +0900

    fix : AndroidLibraryPlugin 에서 코루틴 의존성 제거

--- build-logic/conventions/src/main/kotlin/AndroidLibraryPlugin.kt
+++ build-logic/conventions/src/main/kotlin/AndroidLibraryPlugin.kt
@@ -1,13 +1,10 @@
 import extension.android
-import extension.implementation
 import extension.kotlin
-import extension.library
 import extension.libs
 import extension.version
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.dependencies
 
 internal class AndroidLibraryPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -28,11 +25,6 @@ internal class AndroidLibraryPlugin : Plugin<Project> {
 
             kotlin {
                 jvmToolchain(21)
-            }
-
-            dependencies {
-                implementation(libs.library("kotlinx.coroutines.android"))
-                implementation(libs.library("kotlinx.coroutines.core"))
             }
         }
     }
